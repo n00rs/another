fada73fd58631f5e4fc692a4cbb259579a300ed7
n00rs <007s.noorsha@gmail.com>
2022-12-16 12:58:25 +0530

    configured database

---
+++ server/config/mongodb.js
@@ -0,0 +1,20 @@
+const { MongoClient } = require("mongodb");
+
+const { MONGO_URL } = process.env;
+console.log(MONGO_URL);
+const client = new MongoClient(MONGO_URL);
+const dbName = "bitroot";
+let db;
+
+const connect = async () => {
+  try {
+    const con = await client.connect();
+    console.log(con);
+    db = client.db(dbName);
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = { connect, db };
--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
-const app = require('express')()
-const env = require('dotenv').config()
-const {PORT} = process.env
+const env = require("dotenv").config();
+const express = require("express");
+const { connect } = require("./config/mongodb");
+const app = express();
+const { PORT } = process.env;
 
-app.use(express)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+connect()
 
-app.listen(PORT,()=>console.log(`server running at PORT :${PORT}`))
+
+
+
+app.listen(PORT, () => console.log(`server running at PORT :${PORT}`));
