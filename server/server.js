b21fcf0e750e4af6f249ca9c45cc9d25f9ceff83
n00rs <007s.noorsha@gmail.com>
2022-12-17 20:06:46 +0530

    buld 0.2.8

--- server/server.js
+++ server/server.js
@@ -12,13 +12,13 @@ connect();
 
 app.use("/api/contact", require("./routes/contactMongo"));
 
-if (process.env.NODE_ENV === "production") {
-  // app.use(express.static("client/build"));
-  // app.use(express.static(path.resolve(__dirname, "client", "build")));
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  // });
-}
+// if (process.env.NODE_ENV === "production") {
+//   // app.use(express.static("client/build"));
+//   // app.use(express.static(path.resolve(__dirname, "client", "build")));
+//   // app.get("*", (req, res) => {
+//   //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   // });
+// }
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("*", (req, res) => {
