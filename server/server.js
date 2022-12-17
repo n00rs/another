8be3d3d6740c47cab95212a7efd8aff0b63f5bc3
n00rs <007s.noorsha@gmail.com>
2022-12-17 16:20:52 +0530

    build

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const env = require("dotenv").config();
 const express = require("express");
 const { connect } = require("./config/mongodb");
+const path = require("path");
 const app = express();
 const { PORT } = process.env;
 
@@ -11,8 +12,17 @@ connect();
 
 app.use("/api/contact", require("./routes/contactMongo"));
 
+if (process.env.NODE_ENV === "production") {
+  
+  app.use(express.static("client/build"));
+  app.use(express.static(path.resolve(__dirname, "client", "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+
+}
 app.use((err, req, res, next) => {
-  console.log(err)
+  console.log(err);
   const statusCode = err.statusCode ? err.statusCode : 500;
   res.status(statusCode).json({ message: err.message });
 });
